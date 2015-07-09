a083dfca91b25907a07eaa23d64e29b1a86798b6
Kuronogard <danielpintoriv@gmail.com>
2015-07-09 18:35:37 +0200

    More package fixes...

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name = 'teleg-api-bot',
     packages = ['telegapi'],
-    data_files = [('telegapi',['telegapi/config.yml'])],
-    version = '0.0.11',
+    version = '0.0.111',
     description = 'Wrapper to Telegram bots API',
     author = 'LibreLabUCM',
     author_email = 'librelabucm@googlegroups.com',
     url = 'https://github.com/LibreLabUCM/teleg-api-bot',
+    license = 'GNU General Public License (GPL)',
+
+    include_package_data=True,
+    package_data={
+        '':['config.yaml'],
+
+    },
     install_requires = [
         'Requests',
         'pyyaml'
